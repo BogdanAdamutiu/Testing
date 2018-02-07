8bf21a68e2dcca1db905758736d4814f022d4994
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-02-07 21:01:33 +0100

    Added location for excel file

--- TestingGurukula/src/Tests/Constants.java
+++ TestingGurukula/src/Tests/Constants.java
@@ -4,10 +4,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Constants {
 	
-	//used browsers
-	public static FirefoxDriver Mozila = new FirefoxDriver();
+	//excel location
+	public static final String ExcelLocation = System.getProperty("user.home") + "\\Documents\\Results.xlsx";
+	
+	//initialise browser
+	public static final FirefoxDriver Mozila = new FirefoxDriver();
 	
 	//login information
 	public static final String User = "admin";
 	public static final String Password = "admin";
+	
+	//branch information
+	public static final String Branch = "Software";
+	public static final String Code = "123456";
 }
