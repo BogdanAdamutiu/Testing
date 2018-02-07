238d40ee038f3f9f0ffdb157d94f8adccd3199e8
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-02-07 21:03:28 +0100

    Added call to create excel file

--- TestingGurukula/src/Tests/Main.java
+++ TestingGurukula/src/Tests/Main.java
@@ -1,12 +1,16 @@
 package Tests;
 
+import java.io.IOException;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException, IOException {
 		
 		ReusableAcctions Test = new ReusableAcctions();
-		Test.StartFirefox();
+		Test.ResultsFile(Tests.Constants.ExcelLocation);
+		Test.OpenBrowser();
+		Test.Login(Tests.Constants.User, Tests.Constants.Password);	
+		
 	}
 
 }
