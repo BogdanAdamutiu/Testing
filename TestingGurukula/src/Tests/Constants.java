c730ab92f4316e4c5eaa0e92d6e0b8fd1b6b366a
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-02-07 18:07:09 +0100

    Added constant for Firefox browser

--- TestingGurukula/src/Tests/Constants.java
+++ TestingGurukula/src/Tests/Constants.java
@@ -1,6 +1,11 @@
 package Tests;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Constants {
+	
+	//used browsers
+	public static FirefoxDriver Mozila = new FirefoxDriver();
 	
 	//login information
 	public static final String User = "admin";
