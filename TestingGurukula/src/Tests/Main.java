f2ca889293a9788abbd61a5b187d8eae37d2823a
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-02-08 08:39:52 +0100

    Added call to second test case

--- TestingGurukula/src/Tests/Main.java
+++ TestingGurukula/src/Tests/Main.java
@@ -6,11 +6,24 @@ public class Main {
 
 	public static void main(String[] args) throws InterruptedException, IOException {
 		
-		ReusableAcctions Test = new ReusableAcctions();
-		Test.ResultsFile(Tests.Constants.ExcelLocation);
-		Test.OpenBrowser();
-		Test.Login(Tests.Constants.User, Tests.Constants.Password);	
-		
+//		ReusableAcctions Test1 = new ReusableAcctions();
+		//Test.ResultsFile(Tests.Constants.ExcelLocation);
+//		Test1.OpenBrowser();
+//		Test1.Login(Tests.Constants.User, Tests.Constants.Password);
+		LoginInformation Testing = new LoginInformation();
+		Testing.UserInformation();
+		Testing.RememberLogin();
+//		Test.NavigateToBranch();
+//		Test.CreateBranch(Tests.Constants.Branch, Tests.Constants.Code, 1, 1, 1);
+//		Test.NavigateToStaff();
+//		Test.CreateStaff(Tests.Constants.Staff, 1, 2, 1);
+//		Test.Close();
+//		ReusableAcctions Test2 = new ReusableAcctions();
+//		Test2.OpenBrowser();
+//		Test2.Login(Tests.Constants.User, Tests.Constants.Password);
+//		Test1.Logout(2, 4);
+
+
 	}
 
 }
