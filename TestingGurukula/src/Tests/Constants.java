4f5db7393668368d57aab9566090419ee1ff1e2c
Bogdan Adamutiu <bogdan.adamutiu@gmail.com>
2018-02-08 11:01:09 +0100

    Corected some values

--- TestingGurukula/src/Tests/Constants.java
+++ TestingGurukula/src/Tests/Constants.java
@@ -3,40 +3,39 @@ package Tests;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class Constants {
-	
-	//excel location and excel variables
+	//excel location
 	public static final String ExcelLocation = System.getProperty("user.home") + "\\Documents\\Results.xlsx";
 	public static final XSSFWorkbook workbook = new XSSFWorkbook();
-		
+	
 	//login information
 	public static final String User = "admin";
 	public static final String Password = "admin";
-	
+		
 	//branch information
 	public static final String Branch = "Software";
 	public static final String Code = "123456";
-	
+		
 	//staff information
 	public static final String Staff = "Developer";
-	
+		
 	//new user information
 	public static final String NewUser = "testing";
 	public static final String NewPassword = "testing";
 	public static final String Email = "test@yahoo.com"; 
-	
+		
 	//constants for testing data type
 	public static final String Short = "test";
 	public static final String Upper = "TESTING";
 	public static final String Combined = "1t2e3s4t";
 	public static final String Special = "!@#$%";
 	public static final String Long = "thisusernameshouldbetolongtobeausernamewithmorethen";
-	
+		
 	//constants for testing email address
 	public static final String Incomplete = "test@";
 	public static final String Mistake = "test@yahoo,com";
-	
+		
 	//account information
 	public static final String FirstName = "Administrator";
 	public static final String LastName = "Administrator";
-	public static final String InitialEmail = "admin@localhost";
+	public static final String InitialEmail = "admin@localhost";	
 }
